a833e829f5d135e256e5f6291875f23f8c61b7da
Slobodan Marinkovik <marinkovik.slobodan@gmail.com>
2019-03-31 01:08:06 +0100

    first commit

---
+++ SportLifeScraper/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SportLifeScraper
+{
+    class Program
+    {
+        public static void Scraper()
+        {
+            var currentWinCoef = 1.00;
+            var minimumCoef = 1.90;
+            var totalMaximumCoef = 90.00;
+            List<string> footballResults = new List<string>();
+            IWebDriver driver = new ChromeDriver(@"C:\chromedriverfolder");
+            driver.Url = "http://sportlife.com.mk/Results";
+
+            driver.FindElement(By.XPath("//*[@id='prikaz_select']/option[2]")).Click();
+            System.Threading.Thread.Sleep(2000);
+            var divs = driver.FindElement(By.Id("template-container_0")).FindElements(By.ClassName("result-row"));
+            divs.Sh
+            foreach (var div in divs)
+            {
+                var tmpDiv = div.FindElements(By.ClassName("open-table-1")).ToList();
+                if ((int.Parse(tmpDiv.First().Text.Substring(0, 2)) >= 11) && int.Parse(tmpDiv.First().Text.Substring(0, 2)) < 23)
+                {
+                    try
+                    {
+                        if ((float.Parse(div.FindElement(By.ClassName("select-qu")).Text.ToString(), System.Globalization.CultureInfo.InvariantCulture)) >= minimumCoef)
+                        {
+                            footballResults.Add(tmpDiv[2].Text.ToString() + " vs " + tmpDiv[3].Text.ToString() + " with coeficient " + div.FindElement(By.ClassName("select-qu")).Text.ToString());
+                            currentWinCoef *= (float.Parse(div.FindElement(By.ClassName("select-qu")).Text.ToString(), System.Globalization.CultureInfo.InvariantCulture));
+                            if (currentWinCoef > totalMaximumCoef)
+                            {
+                                footballResults.Add("Vkupno " + Math.Round(currentWinCoef,2) + " koeficient.");
+                                break;
+                            }
+                        }
+                    } catch { }                    
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Scraper();
+            Console.ReadLine();
+        }
+    }
+}
